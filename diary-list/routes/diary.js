8da8aeea4b2e2a5731ccfdf990fb4c0be96b0be6
임영욱 <ywyim@gaia3d.com>
2024-07-10 12:24:18 +0900

    refactor : router 리팩토링 (chainable route 변경)

--- diary-list/routes/diary.js
+++ diary-list/routes/diary.js
@@ -1,77 +1,76 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const db = require('../db-pg');
 const getStringedDate = require('../utils/date');
 
-router.get('/', async (req, res) => {
-	try {
-		const result = await db.query('SELECT id, emotion_id as "emotionId", content, created_date as "createdDate" FROM diary_list');
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Internal Server Error');
-	}
-});
-
-router.post('/', async (req, res) => {
-	const diary = req.body;
-
-	try {
-		const createdDate = getStringedDate(new Date(diary.createdDate));
-		const result = await db.query(`INSERT INTO diary_list (content, emotion_id, created_date) VALUES ('${diary.content}', ${diary.emotionId}, '${createdDate}'::timestamp)`);
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Internal Server Error');
-	}
-});
-
-router.get('/:id', async (req, res) => {
-	const id = req.params.id;
-	console.log(id);
-
-	try {
-		const result = await db.query(`SELECT id, emotion_id as "emotionId", content, created_date as "createdDate" FROM diary_list WHERE id = ${id}`);
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Internal Server Error');
-	}
-});
-
-router.delete('/:id', async (req, res) => {
-	const id = req.params.id;
-	console.log(id);
-
-	try {
-		const result = await db.query(`DELETE FROM diary_list WHERE id = ${id}`);
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Internal Server Error');
-	}
-});
-
-router.patch('/', async (req, res) => {
-	const diary = req.body;
-
-	try {
-		const findDiary = await db.query(`SELECT * FROM diary_list WHERE id = ${diary.id}`);
-
-		if(!findDiary) {
-			console.log('not found diary');
-			return;
+router.route('/')
+	.get(async (req, res) => {
+		try {
+			const result = await db.query('SELECT id, emotion_id as "emotionId", content, created_date as "createdDate" FROM diary_list');
+			res.json(result.rows);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Internal Server Error');
 		}
+	})
+	.post(async (req, res) => {
+		const diary = req.body;
 
-		const createdDate = getStringedDate(new Date(diary.createdDate));
-		const result = await db.query(`UPDATE diary_list SET emotion_id = ${diary.emotionId}, content = '${diary.content}', created_date = '${createdDate}'::timestamp WHERE id = ${diary.id}`)
+		try {
+			const createdDate = getStringedDate(new Date(diary.createdDate));
+			const result = await db.query(`INSERT INTO diary_list (content, emotion_id, created_date) VALUES ('${diary.content}', ${diary.emotionId}, '${createdDate}'::timestamp)`);
+			res.json(result.rows);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Internal Server Error');
+		}
+	})
+	.patch(async (req, res) => {
+		const diary = req.body;
 
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Internal Server Error');
-	}
-})
+		try {
+			const findDiary = await db.query(`SELECT * FROM diary_list WHERE id = ${diary.id}`);
+
+			if(!findDiary) {
+				console.log('not found diary');
+				return;
+			}
+
+			const createdDate = getStringedDate(new Date(diary.createdDate));
+			const result = await db.query(`UPDATE diary_list SET emotion_id = ${diary.emotionId}, content = '${diary.content}', created_date = '${createdDate}'::timestamp WHERE id = ${diary.id}`)
+
+			res.json(result.rows);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Internal Server Error');
+		}
+	});
+
+router
+	.route('/:id')
+	.get(async (req, res) => {
+		const id = req.params.id;
+		console.log(id);
+
+		try {
+			const result = await db.query(`SELECT id, emotion_id as "emotionId", content, created_date as "createdDate" FROM diary_list WHERE id = ${id}`);
+			res.json(result.rows);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Internal Server Error');
+		}
+	}).delete(async (req, res) => {
+		const id = req.params.id;
+		console.log(id);
+
+		try {
+			const result = await db.query(`DELETE FROM diary_list WHERE id = ${id}`);
+			res.json(result.rows);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Internal Server Error');
+		}
+	});
 
 router.get('/diary', function(req, res) {
 	res.send('diary');
